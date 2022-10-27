2207827daafcb58af7d5f138f93c19446d92d10d
byIx <40878600+byIx@users.noreply.github.com>
2022-10-27 22:50:16 +0600

    Update checker.php

--- checker.php
+++ checker.php
@@ -1,1 +1,1 @@
-{ "version":"1.0.0", "longversion":10000, "released":1664143200, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/111\/" }
+{ "version":"1.0.0", "longversion":10000, "released":1664143200, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/2780-k4-unreact-blocker\/" }
